6ba9db5c2d78a5aafa6eedaf82761f2f87df46aa
Timur Puchinsky <timur.2.puchinsky@gmail.com>
2022-07-24 11:27:15 +0300

    Шаблонизатор Jinja и HTML шаблоны

--- Site/website/main/views.py
+++ Site/website/main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h4>test</h4>")
+    return render(request, 'main/index.html')
 
 def about(request):
-    return HttpResponse("<h4>test 2</h4>")
+    return render(request, 'main/about.html')
